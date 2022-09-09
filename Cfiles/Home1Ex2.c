4d92329cf71a8175e42657e86ad41c5697bc6539
OmarAyman <ommoor642@gmail.com>
2022-09-08 17:30:10 -0800

    H1EX2

---
+++ Cfiles/Home1Ex2.c
@@ -0,0 +1,15 @@
+/*
+ * main.c
+ *
+ *  Created on: Sep 8, 2022
+ *      Author: OMAR_AYMAN
+ */
+#include"stdio.h"
+#include"stdint.h"
+int main(void) {
+	int i ;
+	printf("Enter an integer : ");
+	fflush(stdin);fflush(stdout);
+	scanf("%d",&i);
+	return(0);
+}
